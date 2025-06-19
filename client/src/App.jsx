963896273f3f5d76e1a418c10ebac7328f5862ce
nikhil-177 <pn2683249@gmail.com>
2025-06-19 16:43:15 +0530

    client : removed client

--- client/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react';
-import { AppRoutes } from './routes/AppRoutes';
-
-function App() {
-
-  return <AppRoutes />;
-}
-
-export default App;
--- client/src/store/useResetStore.js
+++
@@ -1,7 +0,0 @@
-import { create } from 'zustand';
-
-export const useResetStore = create((set) => ({
-  resetEmail: null,
-  setResetEmail: (resetEmail = null) => set({ resetEmail }),
-  clearResetEmail: () => set({ resetEmail: null }),
-}));
