54dee8d389b229d05d75385872c7a5424fba3e2a
nikhil-177 <pn2683249@gmail.com>
2025-06-19 17:03:27 +0530

    client setup done

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import { AppRoutes } from './routes/AppRoutes';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <AppRoutes />;
+}
+
+export default App;
